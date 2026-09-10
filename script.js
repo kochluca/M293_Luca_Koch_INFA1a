b381f43ea8566a78d10d6336c8f0ae22b9bbbf11
kochluca <luca.koch.root@icloud.com>
2026-09-10 15:16:51 +0200

    Im unterricht gearbeitet und vorbereitet für zuhause um alles dynamisch zu machen

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //Button um abzusenden
-const myButton = document.querySelector(".SendButton");
-myButton.addEventListener("click", (e) => {
+const myFormular = document.querySelector("form");
+
+myFormular?.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const myFormular = document.querySelector("form");
   const data = new FormData(myFormular);
 
   data.forEach((wert, name) => {
@@ -18,7 +18,67 @@ myButton.addEventListener("click", (e) => {
 
 //Button für Popup schleissung
 const closeButton = document.getElementById("close-Button");
-closeButton.addEventListener("click", () => {
+closeButton?.addEventListener("click", () => {
   const popup = document.getElementById("meinPopup");
   popup.close();
+});
+
+//Produkte und Bilder usw. mit API fetchen
+
+//Smartphones
+
+const getSmartphoneProducts = async () => {
+  const response = await fetch(
+    "https://dummyjson.com/products/category/smartphones",
+  );
+  const data = await response.json();
+
+  const products = data.products;
+  console.log(products);
+
+  const productList = document.querySelector();
+
+  for (let i = 0; i < products.length; i++) {
+    const currentProduct = products[i];
+    console.log(currentProduct);
+    productList.innerHTML = `
+    HTML CODE!!!!!
+    <article class="product-card">
+          <div class="image-box">
+            <img
+              class="phonepictures"
+              src="/assets/smartphone.webp"
+              alt="iPhone 5s"
+            />
+          </div>
+          <div class="product-header">
+            <h2>iPhone 5s</h2>
+            <div class="price-group">
+              <span class="price">649 CHF</span>
+              <img src="/assets/cart.png" class="cart-icon" alt="Warenkorb" />
+            </div>
+          </div>
+          <select name="Speicherplatz">
+            <option value="" disabled selected hidden>
+              Speicherplatz auswählen
+            </option>
+            <option value="16gb">16 GB</option>
+            <option value="32gb">32 GB</option>
+            <option value="64gb">64 GB</option>
+          </select>
+          <p class="description">
+            The iPhone 5s is a classic smartphone known for its compact design
+            and advanced features during its release. While it's an older model,
+            it still provides a reliable user experience.
+          </p>
+        </article>
+    `;
+    //Für jedes produkt neue kachel und wie daten dynamisch angezeigt werden sind HAUSAUFGABE          !!!!!!!!!!!!!!!
+  }
+};
+
+//führt den JavaScript Code aus
+
+document.addEventListener("DOMContentLoaded", () => {
+  getSmartphoneProducts();
 });
